2f381437d134b109b089d76d64cd021150577c66
Muskan15-debug <muskandalsania7@gmail.com>
2026-04-07 00:00:57 +0530

    testing

---
+++ tests/contract-test.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || "http://localhost:5050";
+
+async function runTest() {
+    try {
+        const res = await fetch(`${USER_SERVICE_URL}/user/1`);
+        const data = await res.json();
+        console.log("Received: ", data);
+        if (!data.id || !data.name || !data.email) {
+            console.error("Contract Broken!");
+            process.exit(1);
+        }
+
+        console.log("Contract valid");
+        process.exit(0);
+    } catch (err) {
+        console.error("User contract test failed", err.message);
+        process.exit(1);
+    }
+}
+
+runTest();
